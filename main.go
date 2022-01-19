8293b31ee9a779ede1aef814aecb48d79fd473ec
Timothy Odebunmi <dev.brymes@gmail.com>
2022-01-19 12:29:47 +0100

    Main Function

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"scraper/scraper"
+	"scraper/utils"
+)
+
+func main() {
+	//Launch Chrome
+	utils.LaunchChrome()
+
+	// Launch Main Scraper entry point function
+	scraper.Scraper()
+}
