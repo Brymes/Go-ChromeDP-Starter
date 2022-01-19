144c33d6cf026075641452332082942143e61782
Timothy Odebunmi <dev.brymes@gmail.com>
2022-01-19 12:30:16 +0100

    Scraper Entry Point

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,1 +1,23 @@
 package scraper
+
+import (
+	"scraper/utils"
+	"time"
+
+	"github.com/chromedp/chromedp"
+)
+
+func Scraper() {
+	// Pass in the amount of seconds for Chrome Context to lie
+	ctxt, _ := utils.GenerateChromeContext(180)
+
+	url := ""
+
+	task1 := chromedp.Tasks{
+		chromedp.Navigate(url),
+		chromedp.Sleep(time.Second * 30),
+		// Do something else
+	}
+
+	chromedp.Run(ctxt, task1)
+}
