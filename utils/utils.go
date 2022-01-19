06ce4d1f603b0ee6de0a3fdd8a1904a4c1f987ff
Timothy Odebunmi <dev.brymes@gmail.com>
2022-01-19 12:17:22 +0100

    Generate Chrome Context

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,31 @@
 package utils
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/chromedp/chromedp"
+)
+
+var devtoolsWsURL = flag.String("devtoolsWSurl", "http://127.0.0.1:9222/", "DevTools Websocket URL")
+
+func GenerateChromeContext(timed int64) (ctxt context.Context, cancelFunc context.CancelFunc) {
+	var allocatorContext context.Context
+	allocatorContext, _ =
+		chromedp.NewRemoteAllocator(context.Background(),
+			*devtoolsWsURL)
+
+	ctx, _ := chromedp.NewContext(allocatorContext)
+
+	dur, err := time.ParseDuration(fmt.Sprintf("%vs", timed))
+	if err != nil {
+		log.Fatalf("Error Generating Chrome Context ::: %v", err)
+	}
+
+	ctxt, cancelFunc = context.WithTimeout(ctx, time.Duration(dur))
+
+	return
+}
